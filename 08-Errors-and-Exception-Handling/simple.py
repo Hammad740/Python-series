96935c6624e08fb8819c3ee0acc0d442274708d3
Hammad740 <mohammadhammadansari91@gmail.com>
2024-04-03 23:18:33 +0530

    pylint and unittesting

---
+++ 08-Errors-and-Exception-Handling/simple.py
@@ -0,0 +1,19 @@
+# a = 10
+# b = 20
+# print(a)
+# print(B)
+
+# ? command for pylint is : pylint simple.py -r y
+
+
+def myfunc():
+    '''
+    a  simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+
+myfunc()
